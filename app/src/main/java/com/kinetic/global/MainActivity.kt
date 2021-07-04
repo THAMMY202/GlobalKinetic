a88386792de9a46feed48841195036b4a912f58c
Thamsanqa Shabalala <thammy202@gmail.com>
2021-07-04 17:00:48 +0200

    Cleaned code

--- app/src/main/java/com/kinetic/global/MainActivity.kt
+++ app/src/main/java/com/kinetic/global/MainActivity.kt
@@ -57,57 +57,6 @@ class MainActivity : AppCompatActivity(), LocationListener {
         }
     }
 
-    private fun getWeatherByLocation(location: Location) {
-        //TODO Check location values , hit api and conver respond
-        //val url ="https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=636e6df4d5c9c1d14640e9ff8a629364"
-        val url = "https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=${Constants.API_KEY}"
-
-        //init items
-      /*  val textViewCurrentTemp: TextView = findViewById(R.id.temperature_text)
-        val textViewMaxTemp: TextView = findViewById(R.id.max_text)
-        val textViewMinTemp: TextView = findViewById(R.id.min_text)
-        val textViewFeelTemp: TextView = findViewById(R.id.feeling_text)
-
-        val request = JsonObjectRequest(
-                Request.Method.GET, // method
-                url, // url
-                null, // json request
-                { response -> // response listener
-                    try {
-                        val obj: JSONObject = response
-                        val json = obj.getJSONObject("main")
-                        val gson = Gson()
-
-                        var placeTemp: Main = gson.fromJson(json.toString(), Main::class.java)
-                        Log.i("Place Temperature", placeTemp.temp.toString())
-
-                        textViewCurrentTemp.text = ("${placeTemp.temp} ° ")
-                        textViewMaxTemp.text = ("Max :  ${placeTemp.tempMax} ° ")
-                        textViewMinTemp.text = ("Min :  ${placeTemp.tempMin} ° ")
-                        textViewFeelTemp.text = ("it feels like ${placeTemp.feelsLike} ° ")
-
-                        spinner?.visibility = View.GONE
-
-
-                    } catch (e: JSONException) {
-                        Log.i("Place Temperature", e.toString())
-                        Toast.makeText(applicationContext, e.toString(), Toast.LENGTH_SHORT).show()
-                        spinner?.visibility = View.GONE
-                    }
-
-                },
-                { error -> // error listener
-                    Log.i("Place Temperature", error.toString())
-                    Toast.makeText(applicationContext, "City name is incorrect", Toast.LENGTH_SHORT)
-                            .show()
-
-                }
-        )
-
-        // add network request to volley queue
-        VolleySingleton.getInstance(applicationContext).addToRequestQueue(request)*/
-    }
-
     private fun getLocation() {
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         if ((ContextCompat.checkSelfPermission(
@@ -220,10 +169,12 @@ class MainActivity : AppCompatActivity(), LocationListener {
                 findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
 
                 reload = false
+                Log.i("API Exception", result.toString())
 
             } catch (e: Exception) {
                 findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
                 findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
+                Log.i("API Exception", e.toString())
             }
 
         }
